d16bdba048c010d4bbb3d4a7b82d138a8d47df93
Jack Brayan <122466921+jackbrayan17@users.noreply.github.com>
2025-04-30 12:29:45 +0100

    Update wsgi.py

--- stoqtrack/wsgi.py
+++ stoqtrack/wsgi.py
@@ -11,9 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-if os.environ.get('DJANGO_ENV') == 'production':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stoqtrack.settings.production')
-else:
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stoqtrack.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stoqtrack.settings')
 
 application = get_wsgi_application()
