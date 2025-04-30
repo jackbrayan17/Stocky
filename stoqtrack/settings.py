841bd144b25e3773cc4df6d53215aeea8fba9d09
Jack Brayan <122466921+jackbrayan17@users.noreply.github.com>
2025-04-30 13:23:51 +0100

    Update settings.py

--- stoqtrack/settings.py
+++ stoqtrack/settings.py
@@ -118,7 +118,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+import os
+
+STATIC_URL = '/static/'
+
+# Absolute path where collected static files will be stored
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# If you have user-uploaded media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
