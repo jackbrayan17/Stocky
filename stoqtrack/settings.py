0a2174a5751e87069e0a748294aa981616792143
Jack Brayan <122466921+jackbrayan17@users.noreply.github.com>
2025-04-30 13:25:02 +0100

    Update settings.py

--- stoqtrack/settings.py
+++ stoqtrack/settings.py
@@ -44,6 +44,7 @@ LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = 'login'
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
