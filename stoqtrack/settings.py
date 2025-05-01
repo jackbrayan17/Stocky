0d905f3dff3ebe623402d669f2031025a363de23
Jack Brayan <122466921+jackbrayan17@users.noreply.github.com>
2025-05-01 22:27:20 +0100

    Update settings.py

--- stoqtrack/settings.py
+++ stoqtrack/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-#5z9+sr)g5d=v*zprmw-2)3*)$!7ug*6i0(ahig8)b2#3)#s*g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['stocky-place.onrender.com']
 
 
 # Application definition
